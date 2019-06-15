676422cc799b8385f6187297383346be16723e6b
Vladimir Goryavin <49291993+vagoryavin@users.noreply.github.com>
2019-06-15 22:04:25 +0300

    Learning project "13. NSString"

---
+++ 13. NSString/StringsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StringsTest
+//
+//  Created by Горявин Владимир on 21.01.2019.
+//  Copyright © 2019 Goryavin Vladimir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 13. NSString/StringsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StringsTest
+//
+//  Created by Горявин Владимир on 21.01.2019.
+//  Copyright © 2019 Goryavin Vladimir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
